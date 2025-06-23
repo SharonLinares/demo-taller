7f61051b9258d35f63cd31dbd82510db57db17f8
Sharon Linares <akiralyn02@gmail.com>
2025-06-23 12:22:59 +0200

    first commit

---
+++ src/main/java/com/example/demo/DemoTallerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTallerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTallerApplication.class, args);
+	}
+
+}
