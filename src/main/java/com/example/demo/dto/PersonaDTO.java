9b5a8cb56c5cc5f292d81209846807920e5c9ea5
Sharon Linares <akiralyn02@gmail.com>
2025-06-23 12:39:53 +0200

    primer commit

--- src/main/java/com/example/demo/dto/PersonaDTO.java
+++ src/main/java/com/example/demo/dto/PersonaDTO.java
@@ -2,4 +2,43 @@ package com.example.demo.dto;
 
 public class PersonaDTO {
 
+	private String nie;
+	private String nombre;
+	private String linares;
+
+	public PersonaDTO() {
+		super();
+	}
+
+	public PersonaDTO(String nie, String nombre, String linares) {
+		super();
+		this.nie = nie;
+		this.nombre = nombre;
+		this.linares = linares;
+	}
+
+	public String getNie() {
+		return nie;
+	}
+
+	public void setNie(String nie) {
+		this.nie = nie;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getLinares() {
+		return linares;
+	}
+
+	public void setLinares(String linares) {
+		this.linares = linares;
+	}
+
 }
